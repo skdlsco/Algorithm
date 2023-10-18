bee212f05bcf4254f0a3726ff935388a2dd3a528
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:35:04 +0900

    add BOJ 15894

---
+++ baekjoon/15894.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+    val N = reader.readLine().toLong()
+    writer.write("${N * 4}\n")
+    writer.flush()
+}
