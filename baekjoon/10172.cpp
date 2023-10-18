f3c3c331c96f4bade18002a6cb63f8d2a25ca503
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:34:39 +0900

    add BOJ 10172

---
+++ baekjoon/10172.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(void)
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|");
+	return (0);
+}
