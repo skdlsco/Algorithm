9821a2d7fcb3be847cf51eb08308ec790de9f058
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:34:14 +0900

    add BOJ 2231

---
+++ baekjoon/2231.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val str = reader.readLine()
+    val N = str.toLong()
+
+    for (i in str.length * 9 downTo 0) {
+        var M = N - i
+        var sum = M
+        while (M > 0) {
+            sum += M % 10
+            M /= 10L
+        }
+        if (sum == N) {
+            println(N - i)
+            return
+        }
+    }
+    println(0)
+}
