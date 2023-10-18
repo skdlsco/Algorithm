790f888b57956087b2d209968ebe4806a79d3dce
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:34:17 +0900

    add BOJ 2439

---
+++ baekjoon/2439.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n - 1; j++)
+			printf(" ");
+		for (int j = 0; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	return (0);
+}
