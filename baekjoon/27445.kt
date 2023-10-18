7807a87c8bf0e4a75f3c5d1c661ae40260ec0ca6
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:35:26 +0900

    add BOJ 27445

---
+++ baekjoon/27445.kt
@@ -0,0 +1,36 @@
+package A
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.abs
+
+fun checkCanExist(map: Array<Array<Boolean>>, distance: Int, N: Int, M: Int, y: Int, x: Int) {
+    for (gy in 0 until N) {
+        for (gx in 0 until M) {
+            if (abs(gy - y) + abs(gx - x) != distance)
+                map[gy][gx] = false
+        }
+    }
+}
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val (N, M) = reader.readLine().split(" ").map { it.toInt() }
+    val map = Array<Array<Boolean>>(N) { Array(M) { true } }
+
+    repeat(N - 1) {
+        val distance = reader.readLine().toInt()
+        checkCanExist(map, distance, N, M, it, 0)
+    }
+    reader.readLine().split(" ").map { it.toInt() }.forEachIndexed { x, distance ->
+        checkCanExist(map, distance, N, M, N - 1, x)
+    }
+    for (y in 0 until N) {
+        for (x in 0 until M) {
+            if (map[y][x]) {
+                println("${y + 1} ${x + 1}")
+                return
+            }
+        }
+    }
+}
