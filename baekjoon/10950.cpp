c624b3db068e5ce89bf4526a0f3d91cb60b8e61a
skdlsco <nicnic4102@gmail.com>
2023-10-18 10:34:49 +0900

    add BOJ 10950

---
+++ baekjoon/10950.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main(void)
+{
+	int t;
+
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++)
+	{
+		int a, b;
+
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	return (0);
+}
