ba21f17dd8d7127b01b4b4694638a071b0519f32
skdlsco <nicnic4102@gmail.com>
2024-08-15 14:10:27 +0900

    add BOJ 24724

---
+++ baekjoon/24724.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main() {
+  cin >> T;
+  for (int i = 1; i <= T; i++) {
+    cout << "Material Management " << i << "\n";
+    cout << "Classification ---- End!\n";
+  }
+}
