5c275c5261eea1c25217a7dccfd072f470b5fb09
skdlsco <nicnic4102@gmail.com>
2024-08-15 14:07:36 +0900

    add BOJ 2441

---
+++ baekjoon/2441.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int N;
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++){
+            cout << ' ';
+            }
+        for (int j = 0; j < N - i; j++){
+            cout << '*';
+            }
+        
+    cout<<'\n';
+    }
+    return 0;
+}
